9483e6e52c08e2db404928cb5928d062d26afab0
KenanHavic <havickenan3@gmail.com>
2023-06-15 19:38:30 +0200

    Simple QuizApp

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,24 +2,25 @@ package com.example.quizapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnStart: Button = findViewById(R.id.btn_start)
-        val etName: EditText = findViewById(R.id.et_name)
-        btnStart.setOnClickListener {
-            if (etName.text.isEmpty()) {
+        binding.btnStart.setOnClickListener {
+            val name = binding.etName.text.toString()
+            if (name.isEmpty()) {
                 Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
             } else {
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
-                intent.putExtra(Constants.USER_NAME,etName.text.toString())
+                intent.putExtra(Constants.USER_NAME, name)
                 startActivity(intent)
                 finish()
             }
--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -1,28 +1,25 @@
 package com.example.quizapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val tvName: TextView = findViewById(R.id.tv_name)
-        val tvScore: TextView = findViewById(R.id.tv_score)
-        val btnFinish: Button = findViewById(R.id.btn_finish)
+        binding.tvName.text = intent.getStringExtra(Constants.USER_NAME)
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
-        tvName.text = intent.getStringExtra(Constants.USER_NAME)
-        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
-        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
-
-        tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
-        btnFinish.setOnClickListener {
-            startActivity(Intent(this,MainActivity::class.java))
+        binding.tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
+        binding.btnFinish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
-
     }
 }
